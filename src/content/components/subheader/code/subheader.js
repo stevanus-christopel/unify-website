43c7bd06483ad2f2edc2f63d6dbd1be7ecd25f73
Stevanus Christopel <me@stevanus.id>
2018-06-06 16:05:24 +0700

    Fix style

--- src/content/components/subheader/code/subheader.js
+++ src/content/components/subheader/code/subheader.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Subheader from 'unify-react-mobile/build/Subheader';
 
 const ComponentDemo = (
-  <Subheader>
-    Subheader Text
-  </Subheader>
+  <div style={{ backgroundColor:"#FFFFFF", padding: "16px", width: "320px", height: "200px", borderRadius: "4px" }}>
+    <Subheader>
+      Subheader Text
+    </Subheader>
+  </div>
 )
 
 export default ComponentDemo;
